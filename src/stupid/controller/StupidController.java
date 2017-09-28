578b891a814a399246e344820fb3c3b62f6d6806
Dakota Weber <dakota.weber01@gmail.com>
2017-09-28 12:42:36 -0600

    Start method, a singular println

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -1,6 +1,17 @@
 package stupid.controller;
-
+/**
+ * 
+ * @author Dakota Weber
+ *
+ */
 public class StupidController
 {
 
-}
+public void start()
+{
+System.out.println("I get to perform for the football game")
+
+
+
+
+
--- src/stupid/controller/StupidRunner.java
+++ src/stupid/controller/StupidRunner.java
@@ -2,5 +2,8 @@ package stupid.controller;
 
 public class StupidRunner
 {
+public static void main(string [] args)
+StupidController app = new StupidController
+app.start();
 
 }
